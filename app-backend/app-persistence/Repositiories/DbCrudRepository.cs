23eca75db69629e819db07a02351f5a6dec258fc
Pablo Acosta <pablodaa@outlook.com>
2020-10-31 11:47:15 -0700

    added repository implementations

---
+++ app-backend/app-persistence/Repositiories/DbCrudRepository.cs
@@ -0,0 +1,20 @@
+﻿using AppPersistence.Repositiories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppPersistence.Repositiories
+{
+    /// <summary>
+    /// Repository for basic CRUD operations on a given entity
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
+    /// <typeparam name="TPrimaryKey"></typeparam>
+    public class DbCrudRepository<TEntity, TPrimaryKey> : IReadOnlyRepository<TEntity, TPrimaryKey>, IWriteOnlyRepository<TEntity, TPrimaryKey>
+        where TEntity : class
+    {
+        public DbCrudRepository(
+
+            )
+    }
+}
---
+++ app-backend/app-persistence/Repositiories/DbRepository.cs
@@ -0,0 +1,23 @@
+﻿using app_persistence;
+using System;
+using System.Threading.Tasks;
+
+namespace AppPersistence.Repositiories
+{
+    /// <summary>
+    /// Repository for running database operations
+    /// </summary>
+    public class DbRepository
+    {
+        public DbRepository(Func<Db> dbFactory)
+        {
+            this.dbFactory = dbFactory;
+        }
+
+        /// <summary>
+        /// Function for obtaining database sessions
+        /// </summary>
+        private readonly Func<Db> dbFactory;
+
+    }
+}
