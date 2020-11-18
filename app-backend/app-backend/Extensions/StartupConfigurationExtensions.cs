266ae9c161c8109c9560334063ae59a3db7d4725
Pablo Acosta <pablodaa@outlook.com>
2020-11-17 23:19:38 -0700

    Added startup extension config

---
+++ app-backend/app-backend/Extensions/StartupConfigurationExtensions.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace app_backend.Extensions
+{
+    /// <summary>
+    /// Extension methods used in Startup class
+    /// </summary>
+    internal static class StartupConfigurationExtensions
+    {
+        public static void ConfigureCors(this IApplicationBuilder app)
+        {
+            app.UseCors(builder =>
+            {
+                builder.SetIsOriginAllowed(origin =>
+                    // TODO: add strict CORS config
+                    true ||
+                    origin.Contains("http://localhost:")
+                );
+
+                builder.AllowAnyMethod();
+
+                builder.AllowAnyHeader();
+
+                // CREDENTIALS MUST BE ENABLED FOR USING SIGNALR, EVEN WHEN AUTHENTICATION IS NOT USED
+                builder.AllowCredentials();
+            });
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="connectionString"></param>
+        public static void AddBusinessDependencies(
+            this IServiceCollection services,
+            string connectionString
+            )
+        {
+            // DbContext creation
+
+        }
+    }
+}
